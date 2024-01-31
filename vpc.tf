65dae914eac01100dba4ff51fc442decc66c2738
Andrei Burd <aburd@hashicorp.com>
2024-01-31 10:45:55 +0000

    work!

--- vpc.tf
+++ vpc.tf
@@ -7,30 +7,30 @@ module "vpc" {
   azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  default_security_group_egress = [
-    {
-      protocol    = "All"
-      from_port   = 0
-      to_port     = 0
-      cidr_blocks = "0.0.0.0/0"
-    }
-  ]
+  # default_security_group_egress = [
+  #   {
+  #     protocol    = "All"
+  #     from_port   = 0
+  #     to_port     = 0
+  #     cidr_blocks = "0.0.0.0/0"
+  #   }
+  # ]
 
-  default_security_group_ingress = [
-    {
-      protocol    = "tcp"
-      from_port   = 22
-      to_port     = 22
-      cidr_blocks = "0.0.0.0/0"
-      description = "SSH"
-    },
-    {
-      protocol    = "tcp"
-      from_port   = 9200
-      to_port     = 9202
-      cidr_blocks = "0.0.0.0/0"
-      description = "Boundary"
-  }]
+  # default_security_group_ingress = [
+  #   {
+  #     protocol    = "tcp"
+  #     from_port   = 22
+  #     to_port     = 22
+  #     cidr_blocks = "0.0.0.0/0"
+  #     description = "SSH"
+  #   },
+  #   {
+  #     protocol    = "tcp"
+  #     from_port   = 9200
+  #     to_port     = 9202
+  #     cidr_blocks = "0.0.0.0/0"
+  #     description = "Boundary"
+  # }]
 
   enable_nat_gateway = true
   enable_vpn_gateway = false
--- workers.tf
+++ workers.tf
@@ -31,11 +31,34 @@ resource "aws_key_pair" "generated_key" {
   public_key = tls_private_key.playground.public_key_openssh
 }
 
+resource "aws_security_group" "public_worker" {
+  name        = "public_worker"
+  description = "Allow inbound traffic and all outbound traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  tags = {
+    Name = "public worker"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "public_worker_allow_in" {
+  security_group_id = aws_security_group.public_worker.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
+resource "aws_vpc_security_group_egress_rule" "public_worker_allow_out" {
+  security_group_id = aws_security_group.public_worker.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+
 resource "aws_instance" "public_worker" {
   ami                         = data.aws_ami.base.image_id
   instance_type               = "t3.micro"
   associate_public_ip_address = true
-  vpc_security_group_ids      = [module.vpc.default_security_group_id]
+  vpc_security_group_ids      = [aws_security_group.public_worker.id]
   key_name                    = aws_key_pair.generated_key.key_name
   subnet_id                   = module.vpc.public_subnets[0]
   tags = {
@@ -47,11 +70,34 @@ output "ssh_to_public_worker" {
   value = "\nssh -i id_rsa.playground ubuntu@${aws_instance.public_worker.public_ip}\n"
 }
 
+
+resource "aws_security_group" "private_worker" {
+  name        = "private_worker"
+  description = "Allow inbound traffic and all outbound traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  tags = {
+    Name = "private worker"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "private_worker_allow_in" {
+  security_group_id            = aws_security_group.private_worker.id
+  referenced_security_group_id = aws_security_group.public_worker.id
+  ip_protocol                  = "-1"
+}
+
+resource "aws_vpc_security_group_egress_rule" "private_worker_allow_out" {
+  security_group_id = aws_security_group.private_worker.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
 resource "aws_instance" "private_worker" {
   ami                         = data.aws_ami.base.image_id
   instance_type               = "t3.micro"
   associate_public_ip_address = false
-  vpc_security_group_ids      = [module.vpc.default_security_group_id]
+  vpc_security_group_ids      = [aws_security_group.private_worker.id]
   key_name                    = aws_key_pair.generated_key.key_name
   subnet_id                   = module.vpc.private_subnets[0]
   tags = {
@@ -60,11 +106,34 @@ resource "aws_instance" "private_worker" {
 }
 
 
+
+resource "aws_security_group" "innocent_target" {
+  name        = "innocent_target"
+  description = "Allow inbound traffic and all outbound traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  tags = {
+    Name = "private worker"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "innocent_target_allow_in" {
+  security_group_id            = aws_security_group.innocent_target.id
+  referenced_security_group_id = aws_security_group.private_worker.id
+  ip_protocol                  = "-1"
+}
+
+resource "aws_vpc_security_group_egress_rule" "innocent_target_allow_out" {
+  security_group_id = aws_security_group.innocent_target.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
 resource "aws_instance" "innocent_target" {
   ami                         = data.aws_ami.base.image_id
   instance_type               = "t3.micro"
   associate_public_ip_address = false
-  vpc_security_group_ids      = [module.vpc.default_security_group_id]
+  vpc_security_group_ids      = [aws_security_group.innocent_target.id]
   key_name                    = aws_key_pair.generated_key.key_name
   subnet_id                   = module.vpc.private_subnets[1]
   tags = {
